92fbddf267382c103e8e90d14ccd15f577501b9b
sarahadema <SarahAdema@Sarahs-MacBook-Pro.local>
2015-02-09 18:44:29 -0500

    Set Up Document, contained JavaScript information
    Set up a html and javascript documents and linked them together.
    Contained information in js by using objects.

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var blueberries = {
+	family: "Vaccinium Corymbosum",
+	growsOnShrub: true,
+	energy: 240,
+	carbohydrates: 14.49,
+	protein: 0.74,
+}
+
+var grapes = {
+	family: "Vitis Vinifera",
+	growsOnShrub: false,
+	energy: 288,
+	carbohydrates:18.1,
+	protein: 1.4
+}
+
+var redCurrant = {
+	family: "Ribes Rubrum",
+	growsOnShrub: true,
+	energy 234,
+	carbohydrates: 13.8,
+	protein: 1.4,
+}
