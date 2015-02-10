d44e075e13826e37d3e0c9fc47d2ca775ef8d98e
sarahadema <SarahAdema@Sarahs-MacBook-Pro.local>
2015-02-09 19:13:32 -0500

    Fixed how I wrote in information
    Made items into numbers instead of text items

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ var blueberries = {
 	growsOnShrub: true,
 	energy: 240,
 	carbohydrates: 14.49,
-	protein: 0.74,
-}
+	protein: 0.74
+};
 
 var grapes = {
 	family: "Vitis Vinifera",
@@ -12,12 +12,12 @@ var grapes = {
 	energy: 288,
 	carbohydrates:18.1,
 	protein: 1.4
-}
+};
 
 var redCurrant = {
 	family: "Ribes Rubrum",
 	growsOnShrub: true,
 	energy 234,
 	carbohydrates: 13.8,
-	protein: 1.4,
-}
+	protein: 1.4
+};
